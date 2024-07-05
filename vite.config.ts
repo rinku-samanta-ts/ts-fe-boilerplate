55120c5f0bec9f42a931cbefad4dcd39ba499f45
akshaypathak <akshay.pathak@technostacks.com>
2024-07-05 17:57:14 +0530

    feat(setup): validate env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import path from 'path'
 import { defineConfig } from 'vite'
+import { z } from 'zod'
+import { ValidateEnv } from '@julr/vite-plugin-validate-env'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ValidateEnv({
+      validator: 'zod',
+      schema: {
+        VITE_APP_URL: z.string().url('Invalid URL format!'),
+        VITE_COOKIE_BASED_AUTHENTICATION: z.enum(["true","false"]),
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
