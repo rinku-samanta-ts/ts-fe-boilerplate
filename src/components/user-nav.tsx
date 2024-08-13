e4615af790251ea5b4d24ec3f44d46565e242820
Chintan Shah <chintan.shah@technostacks.com>
2024-08-13 14:19:56 +0530

    fix(ui): truncated name and email in profile dropdown

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -34,9 +34,11 @@ export function UserNav() {
       <DropdownMenuContent className='w-56' align='end' forceMount>
         <DropdownMenuLabel className='font-normal'>
           <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>developer</p>
-            <p className='text-xs leading-none text-muted-foreground'>
-              developer@gmail.com
+            <p className='overflow-hidden truncate whitespace-nowrap text-sm font-medium leading-none'>
+              John Doe
+            </p>
+            <p className='overflow-hidden truncate whitespace-nowrap text-xs leading-none text-muted-foreground'>
+              john.doe@yopmail.com
             </p>
           </div>
         </DropdownMenuLabel>
